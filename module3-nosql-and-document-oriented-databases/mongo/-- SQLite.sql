194b84819b33103de04d2bfc7d2989dc811f83b8
jeff kang <jeffyjkang@gmail.com>
2021-11-26 14:09:06 -0600

    assignment3

---
+++ module3-nosql-and-document-oriented-databases/mongo/-- SQLite.sql
@@ -0,0 +1,5 @@
+-- SQLite
+-- select * from charactercreator_character
+
+select * from charactercreator_character_inventory
+-- INNER JOIN armory_item i WHERE i.item_id = c.item_id
--- module3-nosql-and-document-oriented-databases/mongo/mongo.py
+++ module3-nosql-and-document-oriented-databases/mongo/mongo.py
@@ -11,7 +11,6 @@ MONGO_PASSWORD = os.getenv('MONGO_PASSWORD')
 MONGO_DB = os.getenv('MONGO_DB')
 client = pymongo.MongoClient("mongodb+srv://jeff:{}@cluster0.8nwfm.mongodb.net/{}?retryWrites=true&w=majority".format(MONGO_PASSWORD, MONGO_DB))
 db = client[f'{MONGO_DB}']
-print(db)
 
 conn = sqlite3.connect('../../module1-introduction-to-sql/rpg_db.sqlite3')
 curs = conn.cursor()
@@ -31,10 +30,29 @@ def character_doc_creation(db, character_table):
             'strength': character[5],
             'intelligence': character[6],
             'dexterity': character[7],
-            'wisdom': character[8]
+            'wisdom': character[8],
+            'items': [],
+            'weapons': []
         }
+        db.collection.insert_one(character_doc)
+
+def inventory_doc_update(db, inventory_table):
+    for inventory in inventory_table:
+        # (character_id, item_id, item_name)
+        item_type = 'items' if inventory[1] < 138 else 'weapons'
+        db.collection.update_one({'character_id': inventory[0]}, {'$push': {f'{item_type}': inventory[2]}})
+
+SELECT_INVENTORY = '''
+SELECT c.character_id, c.item_id, a.name AS item_name
+FROM charactercreator_character_inventory c
+LEFT JOIN armory_item a
+WHERE c.item_id = a.item_id
+'''
 
 if __name__ == '__main__':
     character_table = execute_query(curs, 'SELECT * FROM charactercreator_character')
     character_doc_creation(db, character_table)
+    inventory_table = execute_query(curs, SELECT_INVENTORY)
+    inventory_doc_update(db, inventory_table)
+
 
